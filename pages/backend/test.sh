abad1859312d8287d703fcca02c98556f643b2d6
GaldCZ <41269537+GaldCZ@users.noreply.github.com>
2020-03-11 12:30:49 +0100

    Update test.sh

--- pages/backend/test.sh
+++ pages/backend/test.sh
@@ -11,4 +11,4 @@ ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 USER=$(cat bitnami_credentials | grep user | awk -F "'" '{print $2}')
 PASS=$(cat bitnami_credentials | grep user | awk -F "'" '{print $4}')
 
-mysql -hdatabase-main.cycq0z9urb9l.eu-west-2.rds.amazonaws.com -P 3307 -u wp-login -pPjhosting@ -e "INSERT INTO wp_logins.first_logins (username, password, instance_id) VALUES('"$USER"', '"$PASS"', '"$ID"');"
+mysql -hdatabaseyxxxxx -P 3307 -u xxx -pxxxx@ -e "INSERT INTO wp_logins.first_logins (username, password, instance_id) VALUES('"$USER"', '"$PASS"', '"$ID"');"
